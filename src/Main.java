7dff0c012bb70c50a5fc25e5ca093a2cf13b4ea7
Flaviu <flaviumarc_cj@yahoo.com>
2019-09-11 20:54:52 +0300

    pro1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        //Dormitor
+        byte walls = 4;
+        byte dors = 2;
+        byte windows = 2;
+        String roomColour = "alb";
+        byte pat = 1;
+        float haine = 220.4f;
+
+
+	// write your code here
+    }
+}
