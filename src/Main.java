fa6a7ca7b5356c58922fa705eb4be53551582f5b
Flaviu <flaviumarc_cj@yahoo.com>
2019-09-17 19:24:18 +0300

    pro1

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,44 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
 
         // Exercitiu 1
+
         System.out.println("Exercitiu 1:");
         System.out.println("Hello\nFlaviu\n");
 
         //Exercitiu 2
-        System.out.println(2+14);
-
-        // Exercitiu 3
+        int min = 2;
+        int max = 100;
+        System.out.println("Exercitiu 2 \nSuna a doua numere aleatorii intre min ("+ min +") si max ("+ max +")" );
+        System.out.println(new Random().nextInt((max - min) + 1) + min);
 
         //Dormitor
         byte walls = 4;
-        byte dors = 2;
+        byte doors = 2;
         byte windows = 2;
         String roomColour = "alb";
         byte pat = 1;
         float haine = 220.4f;
         float luminozitate = 1300.58f;
+        System.out.println("Suma intre walls si doors");
+        System.out.println(walls + doors);
 
-        System.out.println(walls + dors);
+
+        //Exercitiu 3
+        System.out.println("\nExercitiu 3:");
+        System.out.println("Luminozitate + haine = " + luminozitate / haine);
+
+        //Exercitiu 4
+
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55+9) % 9 );
+        System.out.println(20 + -3*5 / 8 );
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3 );
+
+
 
 
 
