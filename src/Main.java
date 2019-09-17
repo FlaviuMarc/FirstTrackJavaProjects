ba711bda052c4809aa8a9ae54a9ee5059a2b8500
Flaviu <flaviumarc_cj@yahoo.com>
2019-09-17 19:25:33 +0300

    pro1

--- src/Main.java
+++ src/Main.java
@@ -32,7 +32,7 @@ public class Main {
         System.out.println("Luminozitate + haine = " + luminozitate / haine);
 
         //Exercitiu 4
-
+        System.out.println("\nExercitiul 4");
         System.out.println(-5 + 8 * 6);
         System.out.println((55+9) % 9 );
         System.out.println(20 + -3*5 / 8 );
