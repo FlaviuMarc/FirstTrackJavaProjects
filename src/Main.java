cda7695e6253339b4e77743e484b3214dc1358d4
Flaviu <flaviumarc_cj@yahoo.com>
2019-09-17 17:14:25 +0300

    pro1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Flaviu Marc");
+        System.out.println("Hello World");
         //Dormitor
         byte walls = 4;
         byte dors = 2;
