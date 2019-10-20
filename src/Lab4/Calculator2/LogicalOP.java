68cc349d0b88da67e87d1b43b656bd30644c7461
Flaviu <flaviumarc_cj@yahoo.com>
2019-10-20 21:44:36 +0300

    pro1

--- src/Lab4/Calculator2/LogicalOP.java
+++ src/Lab4/Calculator2/LogicalOP.java
@@ -2,11 +2,12 @@ package Lab4.Calculator2;
 
 public class LogicalOP {
 
-        public void printBackwards (int nr1, int nr2){
-            for (int i = nr1; i >= nr2; i--){
+    public void printBackwards (int nr1, int nr2){
+        for (int i = nr1; i >= nr2; i--){
             System.out.print(i + ", ");
         }
     }
+
     public float sumAndAverage(){
            float sum = 0;
            float nr = 1;
@@ -18,4 +19,24 @@ public class LogicalOP {
                }
            return (sum / count);
     }
+    public void ex1(int nr1){
+            for (int i = nr1; i<=100; i++)
+                System.out.print(i + " ");
+    }
+    public void ex2(int nr1) {
+        for (int i = nr1; i >= -100; i--) {
+            System.out.print(i + " ");
+        }
+    }
+    public void ex3 (int nr1, int nr2){
+        aa:
+        for (int i = nr1; i <= nr2; i++) {
+            System.out.println(i++);
+            bb:
+            for (int j = nr2; j <= nr1; i++) ;
+            break;
+        }
+
+        }
+
 }
--- src/Lab4/Main.java
+++ src/Lab4/Main.java
@@ -5,7 +5,6 @@ import Lab4.Calculator2.LogicalOP;
 public class Main {
     public static void main(String[] args ){
 
-        System.out.println("Exercitiul clasa 1: ");
 
         LogicalOP exercitiu = new LogicalOP();
         exercitiu.printBackwards(50, 2);
@@ -13,6 +12,18 @@ public class Main {
         System.out.println();
         System.out.println("Exercitiu clasa 2: ");
         System.out.println(exercitiu.sumAndAverage());
+
+        System.out.println("\n Java for loops ex 1: ");
+        exercitiu.ex1(50);
+
+        System.out.println("");
+        System.out.println("\n Java for loops ex 2: ");
+        exercitiu.ex2(80);
+
+        System.out.println("");
+        System.out.println("\n Java for loops ex 3: ");
+        exercitiu.ex3(5,14);
+
 
     }
 }
