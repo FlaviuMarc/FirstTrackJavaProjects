de1ceec22f23e82d3658eb4687b5d7848362ff71
Flaviu <flaviumarc_cj@yahoo.com>
2019-11-04 19:07:46 +0200

    pro1

--- src/Lab5/Calculator/LogicalOP.java
+++ src/Lab5/Calculator/LogicalOP.java
@@ -1,8 +1,72 @@
 package Lab5.Calculator;
 
+import java.lang.reflect.Array;
+
 public class LogicalOP {
+public float sum(int a, int b, float c){
+    return a + b + c;
+}
+public float sum(int a, int b){
+    return a + b;
+}
+public void ex2(){
+    int[] myArray = new int [100];
+    for (int i = 1; i <= 100; i++){
+        myArray[i-1] = i;
+        System.out.println(myArray[i-1]);
+    }
+}
 
+public void ex3(){
+    int myArray[] = new int[100];
+    for (int i = 1; i <=100; i++){
+        if(i % 2 ==0){
+            myArray[i-1] = i;
+            System.out.println(myArray[i-1]);
+        }
+    }
 
+}
 
+public float ex4(int[] myArray ){
+    int sum = 0;
+    int count = 0;
+    int x = 1;
+    while (x <= myArray.length){
+       sum = sum + myArray[x-1];
+        x++;
+        count++;
+    }
+    return sum/count;
+}
+//5. Creati o metoda care sa primeasca un parametru de tip array de string-uri, populat cu valori, si un
+// parametru de tip String. Metoda sa verifice daca valoarea string-ului primit se regaseste in array-ul primit,
+// iar daca da sa returneze true iar daca nu, sa returneze false.  Apelati metoda in main() pentru a verifica daca functioneaza.
 
+public boolean ex5(String[] myArray, String x) {
+    for (int i = 1; i < myArray.length; i++) {
+        if (myArray[i - 1] == x) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return false;
+}
+
+//6. Creati o metoda care sa primeasca un parametru de tip array de numere, populat cu valori,
+// si un parametru de tip numar. Metoda sa verifice daca numarul primit se afla in array-ul primit, si daca da,
+// atunci sa returneze pozitia pe care se afla numarul.  Apelati metoda in main() pentru a verifica daca functioneaza.
+
+public int ex6(int[] myArray, int x){
+    for(int i = 1; i <= myArray.length; i++){
+        if (myArray[i-1]==x){
+            return i-1;
+        }
+        else{
+            return 0;
+        }
+    }
+  return 0;
+}
 }
--- src/Lab5/Calculator/Main.java
+++ src/Lab5/Calculator/Main.java
@@ -1,4 +1,30 @@
 package Lab5.Calculator;
 
 public class Main {
+    public static void main(String[] args) {
+        LogicalOP tema = new LogicalOP();
+
+        System.out.println("Java Arrays ex 1: ");
+        System.out.println(tema.sum(5,8));
+        System.out.println(tema.sum(5,8,2.5f));
+
+        System.out.println("Java Arrays ex 2: ");
+        tema.ex2();
+
+        System.out.println("Java Arrays ex 3: ");
+        tema.ex3();
+
+        System.out.println("Java Arrays ex 4: ");
+        int[] myArray = new int []{1,2,3,4,5,6,7};
+        System.out.println(tema.ex4(myArray));
+
+        System.out.println("Java Arrays ex 5: ");
+        String[] listArray = new String[] {"mama","tata", "caine"};
+        System.out.println(tema.ex5(listArray, "supa"));
+
+        System.out.println("Java Arrays ex 6: ");
+        int[] nrArray = new int[]{2,3,4,5,6,7,8,9,22,14,88};
+        System.out.println(tema.ex6(nrArray, 8));
+    }
 }
+
